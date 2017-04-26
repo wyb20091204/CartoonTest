951211121ec8a480af25d87b954df1e99685bc92
一波 <wuyb@jiahenghealth.com>
2017-04-26 19:07:50 +0800

    init obejct with gitignore

---
+++ CartoonTest/DetailTableViewCell.h
@@ -0,0 +1,14 @@
+//
+//  DetailTableViewCell.h
+//  CartoonTest
+//
+//  Created by 一波 on 2017/4/26.
+//  Copyright © 2017年 一波. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DetailTableViewCell : UITableViewCell
+@property (weak, nonatomic) IBOutlet UIImageView *imgeView;
+
+@end
---
+++ CartoonTest/DetailTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  DetailTableViewController.h
+//  CartoonTest
+//
+//  Created by 一波 on 2017/4/26.
+//  Copyright © 2017年 一波. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DetailTableViewController : UITableViewController
+@property (nonatomic) int chapter;
+@end
